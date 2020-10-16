6de5645e8c09f9b531c0e9dd249352d94d16be23
Omri Zaiman <omri162004@gmail.com>
2020-10-16 16:40:20 +0300

    added buttons to studentMenuActivity

--- app/src/main/java/com/example/testnet/StudentMenuActivity.java
+++ app/src/main/java/com/example/testnet/StudentMenuActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentMenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class StudentMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_menu);
+    }
+
+
+
+
+    public void takeExamClicked(View view) {
+        Intent i = new Intent(StudentMenuActivity.this, JoinExamActivity.class);
+        startActivity(i);
+    }
+
+    public void watchGradesClicked(View view) {
+        Intent i = new Intent(StudentMenuActivity.this, SingleStudentGradesActivity.class);
+        startActivity(i);
+    }
+
+    public void logoutClicked(View view) {
+        //TODO: move back to login screen
     }
 }
