b76fdf487902eeb48bbf1d2fb97e237c929483dc
Omri Zaiman <omri162004@gmail.com>
2020-10-16 16:25:04 +0300

    added buttons to LoginActivity

--- app/src/main/java/com/example/testnet/LoginActivity.java
+++ app/src/main/java/com/example/testnet/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void sendBtnClicked(View view) {
+        //move to students\teachers menu
+
+        Intent i = new Intent(LoginActivity.this, TeacherMenuActivity.class);
+        startActivity(i);
+
+        Intent j = new Intent(LoginActivity.this, StudentMenuActivity.class);
+        startActivity(j);
+    }
+
+    public void signupBtnClicked(View view) {
+        //move to signup activity
+        Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(i);
     }
 }
