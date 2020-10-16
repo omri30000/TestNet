34f606023a5e72b0e880a8a9214f2c5c1405c298
Omri Zaiman <omri162004@gmail.com>
2020-10-16 16:29:39 +0300

    added button to SignupActivity

--- app/src/main/java/com/example/testnet/SignupActivity.java
+++ app/src/main/java/com/example/testnet/SignupActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void sendBtnClicked(View view) {
+        //move to students\teachers menu
+
+        Intent i = new Intent(SignupActivity.this, TeacherMenuActivity.class);
+        startActivity(i);
+
+        Intent j = new Intent(SignupActivity.this, StudentMenuActivity.class);
+        startActivity(j);
     }
 }
