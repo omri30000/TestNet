56f255544ea2e0db31ef1f042bd83cd3319ad2a3
Omri Zaiman <omri162004@gmail.com>
2020-10-17 18:20:34 +0300

    added button to CreateExamActivity

--- app/src/main/java/com/example/testnet/CreateExamActivity.java
+++ app/src/main/java/com/example/testnet/CreateExamActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateExamActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CreateExamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_exam);
+    }
+
+    public void saveBtnClicked(View view) {
+        Intent i = new Intent(CreateExamActivity.this, TeacherMenuActivity.class);
+        startActivity(i);
     }
 }
