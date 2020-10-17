f013a618c3dfb512cccb4930ee4bde961e34891e
Omri Zaiman <omri162004@gmail.com>
2020-10-17 18:28:01 +0300

    added back buttons to SingleStudentGradesActivity and to AllStudentsGradesActivity

--- app/src/main/java/com/example/testnet/AllStudentsGradesActivity.java
+++ app/src/main/java/com/example/testnet/AllStudentsGradesActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /*
 The activity will show the teacher all of his students' grades in a specific exam (get ExamID from intent)
@@ -13,5 +15,10 @@ public class AllStudentsGradesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_students_grades);
+    }
+
+    public void backBtnClicked(View view) {
+        Intent i = new Intent(AllStudentsGradesActivity.this, ExamForResultsActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/testnet/SingleStudentGradesActivity.java
+++ app/src/main/java/com/example/testnet/SingleStudentGradesActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SingleStudentGradesActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SingleStudentGradesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_student_grades);
+    }
+
+    public void backBtnClicked(View view) {
+        Intent i = new Intent(SingleStudentGradesActivity.this, StudentMenuActivity.class);
+        startActivity(i);
     }
 }
