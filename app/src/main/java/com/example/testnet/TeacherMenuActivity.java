03f7071e4643d4ce0fad288dfd00dd9ddcc735bb
Omri Zaiman <omri162004@gmail.com>
2020-10-17 17:52:36 +0300

    added buttons and intents to teacherMenuActivity

--- app/src/main/java/com/example/testnet/TeacherMenuActivity.java
+++ app/src/main/java/com/example/testnet/TeacherMenuActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TeacherMenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class TeacherMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_menu);
+    }
+
+    public void createExamClicked(View view) {
+        Intent i = new Intent(TeacherMenuActivity.this, CreateExamActivity.class);
+        startActivity(i);
+    }
+
+    public void watchGradesClicked(View view) {
+        Intent i = new Intent(TeacherMenuActivity.this, ExamForResultsActivity.class);
+        startActivity(i);
+    }
+
+    public void logoutClicked(View view) {
+        //TODO: move back to login screen
     }
 }
