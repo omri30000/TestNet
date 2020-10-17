be56f0b5b7b404013e03ae63df2ab93b139aec83
Omri Zaiman <omri162004@gmail.com>
2020-10-17 18:14:21 +0300

    added widgets and intents to JoinExamActivity and TakeExamActivity

--- app/src/main/java/com/example/testnet/JoinExamActivity.java
+++ app/src/main/java/com/example/testnet/JoinExamActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class JoinExamActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class JoinExamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_exam);
+    }
+
+    public void sendBtnClicked(View view) {
+        Intent i = new Intent(JoinExamActivity.this, TakeExamActivity.class);
+        //todo: i.putExtras()
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/testnet/TakeExamActivity.java
+++ app/src/main/java/com/example/testnet/TakeExamActivity.java
@@ -2,7 +2,9 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TakeExamActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class TakeExamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_exam);
+    }
+
+    public void submitBtnClicked(View view) {
+        Intent i = new Intent(TakeExamActivity.this, StudentMenuActivity.class);
+        //todo: i.putExtras()
+        startActivity(i);
     }
 }
