d2c60ccd67651b4dc9af11862a2d507f7cc7ac09
Omri Zaiman <omri162004@gmail.com>
2021-01-01 20:10:04 +0200

    updated StartActivity.java file, added option to move between activities automatically.

--- app/src/main/java/com/example/testnet/StartActivity.java
+++ app/src/main/java/com/example/testnet/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //Move to the next activity after X time.
+        int secondsDelayed = 4;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
