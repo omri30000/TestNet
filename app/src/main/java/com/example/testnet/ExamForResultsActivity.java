c000628b1187e8947c3a5b83083a2025575c7232
Omri Zaiman <omri162004@gmail.com>
2021-01-15 20:08:03 +0200

    added logics to ExamForResultsActivity

--- app/src/main/java/com/example/testnet/ExamForResultsActivity.java
+++ app/src/main/java/com/example/testnet/ExamForResultsActivity.java
@@ -2,16 +2,51 @@ package com.example.testnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /*
 The activity will allow a teacher to choose an exam in order to watch his students' results in this exam
 */
 public class ExamForResultsActivity extends AppCompatActivity {
+    private ArrayList<String> testsArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_for_results);
+
+        ListView testsLv = findViewById(R.id.ExForResultsLV);
+        this.testsArr = new ArrayList<String>();
+
+        fillExamsNames();
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.testsArr);
+        testsLv.setAdapter(namesAdapter);
+    }
+
+    public void backBtnClicked(View view) {
+        Intent i = new Intent(ExamForResultsActivity.this, TeacherMenuActivity.class);
+        //todo: i.putExtras()
+        startActivity(i);
+    }
+
+    public void fillExamsNames(){
+        String s = "Exam 1";
+        this.testsArr.add(s);
+
+        s = "Exam 2";
+        this.testsArr.add(s);
+
+        s = "Exam 3";
+        this.testsArr.add(s);
+
+        s = "Exam 4";
+        this.testsArr.add(s);
     }
 }
