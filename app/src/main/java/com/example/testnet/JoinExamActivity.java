72a50be2900c81e0fdb35fdddaf4a0027fc25cd3
Omri Zaiman <omri162004@gmail.com>
2021-01-30 16:01:52 +0200

    updated JoinExamActivity to move user to a exam

--- app/src/main/java/com/example/testnet/JoinExamActivity.java
+++ app/src/main/java/com/example/testnet/JoinExamActivity.java
@@ -5,18 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class JoinExamActivity extends AppCompatActivity {
+    private EditText examIdEt;
+    private Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_exam);
+
+        this.examIdEt = findViewById(R.id.EnterExamIdPT);
+        this.sendBtn = findViewById(R.id.startExamBtn);
     }
 
     public void sendBtnClicked(View view) {
+        String examId = this.examIdEt.getText().toString();
+
+        if (!isExamExist(examId))
+        {
+            Toast t = Toast.makeText(this, "Invalid Exam Identifier!", Toast.LENGTH_SHORT);
+            t.show();
+
+            return;
+        }
+
         Intent i = new Intent(JoinExamActivity.this, TakeExamActivity.class);
-        //todo: i.putExtras()
+        //todo: i.putExtra("userIdentifier", "12345");
+        i.putExtra("examId", examId);
         startActivity(i);
+    }
+
+    /**
+     * The function will check if the given exam id is valid and exists in the database
+     * @param id - the given ID of a specific exam
+     * @return true or false if the exam appears in the DB
+     */
+    private boolean isExamExist(String id)
+    {
+        //todo: check if the ID appears in the database and return the result
+        return true;
     }
 }
