105bbdf3dc2a83ec716c721755e301b49b6b2c8f
Omri Zaiman <omri162004@gmail.com>
2021-03-01 08:09:46 +0200

    updated SingleStudentGradesActivity to use demi-data

--- app/src/main/java/com/example/testnet/SingleStudentGradesActivity.java
+++ app/src/main/java/com/example/testnet/SingleStudentGradesActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SingleStudentGradesActivity extends AppCompatActivity {
+    private ArrayList<String> testsArr;
 
     private String userId;
 
@@ -17,12 +24,40 @@ public class SingleStudentGradesActivity extends AppCompatActivity {
 
         this.userId = (String) getIntent().getExtras().get("userIdentifier");
 
-        //todo: show the student's grades in a list view
+        ListView testsLv = findViewById(R.id.SingleStudentGradesLV);
+        this.testsArr = new ArrayList<String>();
+
+        fillExamsNames();
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.testsArr);
+        testsLv.setAdapter(namesAdapter);
+
+        testsLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //todo: get grade from the database
+                Toast.makeText(SingleStudentGradesActivity.this, "Your grade is: 100", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void backBtnClicked(View view) {
         Intent i = new Intent(SingleStudentGradesActivity.this, StudentMenuActivity.class);
         i.putExtra("userIdentifier", this.userId);
         startActivity(i);
+    }
+
+    public void fillExamsNames(){
+        String s = "Exam 1";
+        this.testsArr.add(s);
+
+        s = "Exam 2";
+        this.testsArr.add(s);
+
+        s = "Exam 3";
+        this.testsArr.add(s);
+
+        s = "Exam 4";
+        this.testsArr.add(s);
     }
 }
