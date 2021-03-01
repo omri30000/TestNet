bd7ee1d93db9f21d588c7f669feb6670d201d564
Omri Zaiman <omri162004@gmail.com>
2021-03-01 07:33:36 +0200

    updated AllStudentsGradesActivity to work independently (except for the db data)

--- app/src/main/java/com/example/testnet/AllStudentsGradesActivity.java
+++ app/src/main/java/com/example/testnet/AllStudentsGradesActivity.java
@@ -5,20 +5,70 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /*
 The activity will show the teacher all of his students' grades in a specific exam (get ExamID from intent)
 */
 public class AllStudentsGradesActivity extends AppCompatActivity {
 
+    private ArrayList<String> namesArr;
+
+    private String userId;
+    private String examName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_students_grades);
+
+        this.userId = (String) getIntent().getExtras().get("userIdentifier");
+        this.examName = (String) getIntent().getExtras().get("examName");
+
+        TextView titleTv = findViewById(R.id.examNameTV);
+        titleTv.setText(this.examName);
+
+        ListView studentsLv = findViewById(R.id.allGradesLV);
+        this.namesArr = new ArrayList<String>();
+
+        fillStudentsNames();
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.namesArr);
+        studentsLv.setAdapter(namesAdapter);
+
+        studentsLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //todo: get grade from the database
+                Toast.makeText(AllStudentsGradesActivity.this, "Student's grade is: 100", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public void backBtnClicked(View view) {
         Intent i = new Intent(AllStudentsGradesActivity.this, ExamForResultsActivity.class);
+        i.putExtra("userIdentifier", this.userId);
         startActivity(i);
+    }
+
+    public void fillStudentsNames(){
+        String s = "Student 1";
+        this.namesArr.add(s);
+
+        s = "Student 2";
+        this.namesArr.add(s);
+
+        s = "Student 3";
+        this.namesArr.add(s);
+
+        s = "Student 4";
+        this.namesArr.add(s);
     }
 }
--- app/src/main/java/com/example/testnet/ExamForResultsActivity.java
+++ app/src/main/java/com/example/testnet/ExamForResultsActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-/*
+/**
 The activity will allow a teacher to choose an exam in order to watch his students' results in this exam
 */
 public class ExamForResultsActivity extends AppCompatActivity {
@@ -33,8 +34,16 @@ public class ExamForResultsActivity extends AppCompatActivity {
         ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.testsArr);
         testsLv.setAdapter(namesAdapter);
 
-        //todo: set on item clicked to the list view
-        //todo: i.putExtra("userIdentifier", this.userId); for item click
+        testsLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(ExamForResultsActivity.this, AllStudentsGradesActivity.class);
+                i.putExtra("userIdentifier", userId);
+                i.putExtra("examName", testsArr.get(position));
+                startActivity(i);
+            }
+        });
+
     }
 
     public void backBtnClicked(View view) {
